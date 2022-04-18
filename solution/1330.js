38decb2ceace3e90a4c003c7272bd6cb6b140056
dongduu <4012popo@naver.com>
2022-04-18 18:33:57 +0900

    feat: solution No.1330

---
+++ solution/1330.js
@@ -0,0 +1,14 @@
+let fs = require("fs");
+
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let a = parseInt(input[0].split(" ")[0]);
+let b = parseInt(input[0].split(" ")[1]);
+
+if (a < b) {
+  console.log("<");
+} else if (a > b) {
+  console.log(">");
+} else {
+  console.log("=");
+}
