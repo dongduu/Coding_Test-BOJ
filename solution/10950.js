7e7fc9cab2fb2b20a7d4159f87ff267331420e31
dongduu <4012popo@naver.com>
2022-04-19 23:35:14 +0900

    feat: solution No.10950

---
+++ solution/10950.js
@@ -0,0 +1,10 @@
+let fs = require("fs");
+
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let n = Number(input[0]);
+
+for (i = 1; i <= n; i++) {
+  let [a, b] = input[i].split(" ").map((x) => Number(x));
+  console.log(a + b);
+}
