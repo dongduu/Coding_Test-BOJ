a27c09ae93f09a8554bcfb01aaf4b4736bd1f383
dongduu <4012popo@naver.com>
2022-05-03 13:04:50 +0900

    feat: insertionSort

--- solution/insertionSort.js
+++ solution/insertionSort.js
@@ -1,13 +1,15 @@
+let arr = [3, 7, 1, 5, 2, 4, 9, 0, 8, 6];
+
 for (let i = 1; i < arr.length; i++) {
   for (let j = i; j > 0; j--) {
-    if (arr[minIndex] > arr[j]) {
-      minIndex = j;
+    if (arr[j] < arr[j - 1]) {
+      let temp = arr[j];
+      arr[j] = arr[j - 1];
+      arr[j - 1] = temp;
+    } else {
+      break;
     }
   }
-  // 스와프(swap)
-  let temp = arr[i];
-  arr[i] = arr[minIndex];
-  arr[minIndex] = temp;
 }
 
 console.log(arr);
