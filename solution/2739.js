5893847f7ce91e84331f46ed0463f8c5564b97d0
dongduu <4012popo@naver.com>
2022-08-16 01:21:05 +0900

    feat: solution No.2739

---
+++ solution/2739.js
@@ -0,0 +1,7 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+let a = Number(input[0].split(" ")[0]);
+
+for (let i = 1; i < 10; i++) {
+  console.log(`${a} * ${i} = ${a * i}`);
+}
