6759d37d8bcbedfe1a3e390ccaae19599bfd39a4
dongduu <4012popo@naver.com>
2022-08-18 14:36:47 +0900

    feat: solution No.10871

--- solution/10871.js
+++ solution/10871.js
@@ -1,15 +1,16 @@
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let n = +input[0].split(" ")[0];
-let max = +input[0].split(" ")[1];
+let count = +input[0].split(" ")[0];
+let num = +input[0].split(" ")[1];
 let arr = input[1].split(" ").map((x) => Number(x));
-let answer = "";
 
-for (let i = 0; i < n; i++) {
-  if (arr[i] < max) {
-    answer += arr[i] + " ";
+let result = "";
+
+for (let i = 0; i < count; i++) {
+  if (arr[i] < num) {
+    result += arr[i] + " ";
   }
 }
 
-console.log(answer);
+console.log(result);
