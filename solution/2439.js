0635b9682f7bd19da33a745771e79c5b971d33e9
dongduu <4012popo@naver.com>
2022-08-18 14:23:42 +0900

    feat: solution No.2439

--- solution/2439.js
+++ solution/2439.js
@@ -5,7 +5,7 @@ let num = Number(input[0].split(" ")[0]);
 let result = "";
 
 for (let i = 0; i < num; i++) {
-  for (let j = 0; j <= num - i - 1; j++) {
+  for (let j = 0; j < num - i - 1; j++) {
     result += " ";
   }
   for (let u = 0; u <= i; u++) {
