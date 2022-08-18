348a7ef8a9c30b14f05c59425d2b040a5e69bb95
dongduu <4012popo@naver.com>
2022-08-18 14:20:16 +0900

    feat: solution No.2439

--- solution/2439.js
+++ solution/2439.js
@@ -1,15 +1,14 @@
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
-
-let n = Number(input[0]);
+let num = Number(input[0].split(" ")[0]);
 
 let result = "";
 
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < n - i - 1; j++) {
+for (let i = 0; i < num; i++) {
+  for (let j = 0; j <= num - i - 1; j++) {
     result += " ";
   }
-  for (let j = 0; j <= i; j++) {
+  for (let u = 0; u <= i; u++) {
     result += "*";
   }
   result += "\n";
