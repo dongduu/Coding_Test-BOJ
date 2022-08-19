404fd843236a3a8e10dca0a9b267a25b899cbcfb
dongduu <4012popo@naver.com>
2022-08-19 15:13:46 +0900

    feat: solution No.1110

--- solution/1110.js
+++ solution/1110.js
@@ -1,18 +1,18 @@
 let fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString().split("\n");
+let input = fs.readFileSync("./index.txt").toString().split("\n");
 let N = parseInt(input[0]);
 
 function solution(N) {
-  let cnt = 0;
   let num = N;
+  let count = 0;
 
   while (true) {
     let sum = parseInt((num % 10) + num / 10);
     num = (num % 10) * 10 + (sum % 10);
-    cnt++;
+    count++;
     if (num === N) break;
   }
-  return cnt;
+  return count;
 }
 
 console.log(solution(N));
