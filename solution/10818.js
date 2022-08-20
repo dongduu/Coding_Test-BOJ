72b1c1da789741a09aea36de082cd0332e32a2f6
dongduu <4012popo@naver.com>
2022-08-21 00:54:56 +0900

    feat: solution No.10818

--- solution/10818.js
+++ solution/10818.js
@@ -1,16 +1,14 @@
 let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split("\n");
-
-let n = Number(input[0]);
+let input = fs.readFileSync("./index.txt").toString().split("\n");
+let N = parseInt(input[0]);
 let arr = input[1].split(" ").map((x) => Number(x));
 
-let maxNum = -1000000;
-let minNum = 1000000;
+let max = -1000000;
+let min = 1000000;
 
-for (let i = 0; i < n; i++) {
-  let currNum = arr[i];
-  if (currNum < minNum) minNum = currNum;
-  if (currNum > maxNum) maxNum = currNum;
+for (let i = 0; i < N; i++) {
+  if (arr[i] < min) min = arr[i];
+  if (arr[i] > max) max = arr[i];
 }
 
-console.log(minNum + " " + maxNum);
+console.log(min, max);
