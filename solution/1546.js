f6a2d1bcc73cac90fec08274aebf319f9d6f4d1c
dongduu <4012popo@naver.com>
2022-08-23 22:08:41 +0900

    feat: solution No.1546

---
+++ solution/1546.js
@@ -0,0 +1,13 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const score = input[1].split(" ").map((x) => Number(x));
+const max = Math.max(...score);
+
+let sum = 0;
+
+for (let i = 0; i < score.length; i++) {
+  sum += (score[i] / max) * 100;
+}
+
+console.log(sum / score.length);
